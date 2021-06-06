1a6ea7266e1082f6a9ba85e475bfaaedbc1b79aa
Bazhukov Max <oa.bazhukov@gmail.com>
2021-06-06 20:54:19 +0300

    changed lang to rus

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 HEADERS = {
-    'authority': 'www.kith.com',
+    # 'authority': 'www.kith.com',
     'cache-control': 'max-age=0',
     'upgrade-insecure-requests': '1',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.106 Safari/537.36',
@@ -8,5 +8,5 @@ HEADERS = {
     'sec-fetch-site': 'same-origin',
     'sec-fetch-mode': 'navigate',
     'sec-fetch-user': '?1',
-    'accept-language': 'en-US,en;q=0.9',
+    'accept-language': 'ru;q=0.9, en-US;q=0.8, en;q=0.7',
 }
